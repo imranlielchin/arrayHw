51bcc91c1c59b2490865467e32c951fd67411622
imranlielchin <elchinii@code.edu.az>
2023-05-07 15:29:49 +0400

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let array = [3 , 6, 2, 56, 32, 5, 89, 32],
+
+    largest = array.sort((a,b)=>a-b).reverse()[0];
+
+    console.log(largest)
+
+
+
+
+let myArr = [1,3,5,7,8];
+
+let myArr2 = myArr.map(x => x*2);
+
+console.log(myArr2)
+
